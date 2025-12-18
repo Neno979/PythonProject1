dc16fb509be411af0f5fabd1f23c7c464572bebe
Neno979 <dr.nenica@gmail.com>
2025-12-18 16:41:46 +0100

    upgrade SecretNumber3 data to dictionary(attempts,time)

--- SecretNumber3.py
+++ SecretNumber3.py
@@ -1,19 +1,23 @@
 import random
 import json
+import datetime
 
 secret = random.randint(1, 30)
 attempts = 0
 
 with open("Score_sheet.json" , "r") as score_file:
     score_data = json.loads(score_file.read())
-    score_data.sort()
-    print(score_data)
+    #score_data.sort()
+    print("all scores: " + str(score_data))
+
+    for score_dict in score_data:
+        print("attempts: " + str(score_dict["attempts"]) + ", time: " + str(score_dict["time"]))
 
 while True:
     guess = int(input("please guess integer number between 1 and 30: "))
     attempts += 1
     if guess == secret:
-        score_data.append(attempts)
+        score_data.append({"attempts": attempts, "time": str(datetime.datetime.now())})
 
         with open("Score_sheet.json", "w") as score_file:
             score_file.write(json.dumps(score_data))
