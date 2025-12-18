b2895a2c7c4f031fd64a3bcb85c598abe9adb157
Neno979 <dr.nenica@gmail.com>
2025-12-18 19:28:15 +0100

    added new data for SecretNumber3 (name,guess num)

--- SecretNumber3.py
+++ SecretNumber3.py
@@ -11,13 +11,13 @@ with open("Score_sheet.json" , "r") as score_file:
     print("all scores: " + str(score_data))
 
     for score_dict in score_data:
-        print("attempts: " + str(score_dict["attempts"]) + ", time: " + str(score_dict["time"]))
-
+        print("Name: " + score_dict["name"] + ", attempts: " + str(score_dict["attempts"]) + ", time: " + str(score_dict["time"]) + ", guessed number " + str(score_dict["guessed_number"]))
+name = input("please enter your name: ")
 while True:
     guess = int(input("please guess integer number between 1 and 30: "))
     attempts += 1
     if guess == secret:
-        score_data.append({"attempts": attempts, "time": str(datetime.datetime.now())})
+        score_data.append({"name": name, "attempts": attempts, "time": str(datetime.datetime.now()), "guessed_number": str(guess) })
 
         with open("Score_sheet.json", "w") as score_file:
             score_file.write(json.dumps(score_data))
