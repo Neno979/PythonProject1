8a87cdf1cecc6ad964aecb5a40fa0980dd0fcfac
Neno979 <dr.nenica@gmail.com>
2026-01-21 23:16:48 +0100

    added templates

--- flask_test.py
+++ flask_test.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return"Hello World"
+    return render_template("index.html")
+
+@app.route("/about-me")
+def about_me():
+    return render_template("about_me.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
