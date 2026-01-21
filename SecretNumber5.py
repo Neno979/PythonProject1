277b67a85258307375a33453d088b5c72227e7dc
Neno979 <dr.nenica@gmail.com>
2026-01-21 19:28:05 +0100

    added reading from json and conversion to list of objects in SecretNumber5.py

--- SecretNumber5.py
+++ SecretNumber5.py
@@ -2,7 +2,7 @@ import random
 import datetime
 import json
 
-class result:
+class Result:
     def __init__(self, name, attempts, wrong, time, guessed_number):
         self.name =name
         self.attempts = attempts
@@ -10,3 +10,13 @@ class result:
         self.time = time
         self.guessed_number = guessed_number
 
+secret = random.randint(1,30)
+Result.attempts = 0
+
+with open("score_sheet.json" , "r") as data_file:
+    score_data_d = json.load(data_file)
+    score_data = [Result(data["name"],data["attempts"], data["wrong"], data["time"], data["guessed_number"]) for data in score_data_d]
+    print(score_data)
+
+for obj in score_data:
+    print(vars(obj))
