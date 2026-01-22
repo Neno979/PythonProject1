a1daef93c68feca1aace6336b70efff322ce7bcd
Neno979 <dr.nenica@gmail.com>
2026-01-22 15:53:58 +0100

    SecretNumber5.py with objects completed

--- SecretNumber5.py
+++ SecretNumber5.py
@@ -11,12 +11,35 @@ class Result:
         self.guessed_number = guessed_number
 
 secret = random.randint(1,30)
-Result.attempts = 0
+att = 0
 
 with open("score_sheet.json" , "r") as data_file:
     score_data_d = json.load(data_file)
     score_data = [Result(data["name"],data["attempts"], data["wrong"], data["time"], data["guessed_number"]) for data in score_data_d]
     print(score_data)
-
+    sorted_score_data = sorted(score_data, key=lambda o: o.attempts)
+    print(sorted_score_data)
 for obj in score_data:
     print(vars(obj))
+for obj in sorted_score_data[:3]:
+    print(vars(obj))
+
+nm = input("Enter your name: ")
+while True:
+    guess = int(input("Please enter integer number between 1 and 30: "))
+    att += 1
+    if guess == secret:
+        t = str(datetime.datetime.now())
+        wr = att -1
+        new_result = Result(nm, att, wr, t, guess)
+        score_data.append(new_result)
+        score_data_d = [score.__dict__ for score in score_data]
+        with open("score_sheet.json" , "w") as data_file:
+            json.dump(score_data_d, data_file)
+        print("Congratulations! You scored in " + str(new_result.attempts) + " attempts!")
+        break
+    elif guess > secret:
+        print("Sorry, you guessed too high! Try lower")
+    elif guess < secret:
+        print("Sorry, you guessed too low! Try higher")
+
